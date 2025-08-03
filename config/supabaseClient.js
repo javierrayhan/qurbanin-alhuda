1b591560dfd26611cd8c4c78ae1cc5ff001e6b8b
javierrayhan <javrayhan2006@gmail.com>
2025-08-03 22:23:37 +0700

    Change Public

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 export const supabase = createClient(
-  'https://lcwxragoszrmlzhetutq.supabase.co',      // Ganti dengan URL dari Supabase Project kamu
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxjd3hyYWdvc3pybWx6aGV0dXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMjU3MDUsImV4cCI6MjA2OTcwMTcwNX0.8lNnybmzGQuk5Z57CMc0KEtxVqvrTTVX-tgVlK7MSjw'                  // Ganti dengan anon key kamu dari Supabase
+  'URL PROJECT',      
+  'ANON KEYS'                  
 )
